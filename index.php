ba4bc681548fdab3dfb9f747faf8e80a681e9721
jhygt46 <infolifeoperacional@gmail.com>
2018-11-18 17:01:31 -0300

    aa

---
+++ index.php
@@ -0,0 +1,83 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <title>FireApp</title>
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+    <script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+    <script type="text/javascript" src="js/anime.min.js"></script>
+    <script type="text/javascript" src="js/base.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Pattaya|Lato" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" media="all" />
+    <style>
+        body{
+            font-family: 'Pattaya', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <div class="contenedor">
+        <div class="valign info">
+            <div class="titulo">
+                <div class="logo valign">Delivery-Shop.cl</div>
+                <ul class="btns valign clearfix">
+                    <li onclick="pagina(0)">Empezar</li>
+                    <li onclick="pagina(1)">Clientes</li>
+                    <li onclick="pagina(2)">Contacto</li>
+                </ul>
+            </div>
+            <div class="data">
+                <div class="paginas pag01">
+                    <div class="pagina">
+                        <div class="infos inp1 valign">
+                            <h1>Crear tu Sitio Web para Delivery Ahora Mismo!</h1>
+                            <h2>Solo debes seguir estos simples pasos</h2>
+                            <h3>Ingresa tu Dominio</h3>
+                            <div class="input"><input type="text" id="dominio" placeholder="www.izusushi.cl"></div>
+                            <h3>Ingresa tu Correo</h3>
+                            <div class="input"><input type="text" id="correo" placeholder="izusushi@gmail.com"></div>
+                            <div class="acciones"><div class="empezar" onclick="send()">Empezar Prueba Gratis</div><div class="mes_gratis">30 dias gratis</div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="paginas pag02">
+                    <div class="pagina">
+                        <div class="infos inp2 valign">
+                            <h1 class="in2pad">Nuestros Clientes</h1>
+                            <div class="lista_clientes clearfix">
+                                <div class='preview' onclick='preview_cliente()'></div>
+                                <div class='show'>
+                                    <div class="cont_show">
+                                        
+                                        
+                                        
+                                    </div>
+                                </div>
+                                <div class='next' onclick='proximo_cliente()'></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="paginas pag03">
+                    <div class="pagina">
+                        <div class="infos inp1 valign">
+                            <h1>Formulario de Contacto</h1>
+                            <h4>&iquest;Te deseas comunicar nos nosotros?</h4>
+                            <h5>Nombre</h5>
+                            <div class="input2"><input type="text" id="dominio" placeholder=""></div>
+                            <h5>Correo</h5>
+                            <div class="input2"><input type="text" id="correo" placeholder=""></div>
+                            <h5>Telefono</h5>
+                            <div class="input2"><input type="text" id="correo" placeholder=""></div>
+                            <h5>Asunto</h5>
+                            <div class="input2"><Textarea id="correo"></Textarea></div>
+                            <div class="enviar_contacto"><div onclick="contacto()">Enviar</div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="valign telefono">
+            <div class="iframe"><iframe id="telefono" src="http://www.mikasushi.cl" width="274" height="486" frameborder="0"></iframe></div>
+        </div>
+    </div>  
+</body>
+</html>
---
+++ js/base.js
@@ -0,0 +1,129 @@
+$(document).ready(function(){
+    
+    render();
+
+});
+
+var clientes = [{ logo: 'mika.jpg', nombre: 'Mika Sushi 01', url: 'www.mikasushi.cl' }, { logo: 'runa.jpg', nombre: 'Runa Sushi 02', url: 'www.runasushi.cl' }, { logo: 'runa.jpg', nombre: 'Runa Sushi 03', url: 'www.mikasushi.cl' }, { logo: 'runa.jpg', nombre: 'Runa Sushi 04', url: 'www.mikasushi.cl' }, { logo: 'mika.jpg', nombre: 'Mika Sushi 05', url: 'www.mikasushi.cl' }, { logo: 'runa.jpg', nombre: 'Runa Sushi 06', url: 'www.mikasushi.cl' }, { logo: 'runa.jpg', nombre: 'Runa Sushi 07', url: 'www.mikasushi.cl' }, { logo: 'runa.jpg', nombre: 'Runa Sushi 08', url: 'www.mikasushi.cl' }, { logo: 'mika.jpg', nombre: 'Mika Sushi 09', url: 'www.mikasushi.cl' }, { logo: 'runa.jpg', nombre: 'Runa Sushi 10', url: 'www.mikasushi.cl' }, { logo: 'runa.jpg', nombre: 'Runa Sushi 11', url: 'www.mikasushi.cl' }, { logo: 'runa.jpg', nombre: 'Runa Sushi 12', url: 'www.mikasushi.cl' }];
+
+function render(){
+    
+    var html = '';
+    for(var i=0; i<clientes.length; i=i+4){
+    
+        html = '<div class="list_cli">'+
+                    '<ul class="clearfix">'+
+                        '<li onclick="iframe(\''+clientes[i].url+'\')"><div class="cliente valign"><div style="background: #fff"><img style="width: 100%" src="http://'+clientes[i].url+'/images/logos/'+clientes[i].url+'.jpg"></div><div>'+clientes[i].nombre+'</div></div></li>'+
+                        '<li onclick="iframe(\''+clientes[i+1].url+'\')"><div class="cliente valign"><div style="background: #000"><img style="width: 100%" src="http://'+clientes[i+1].url+'/images/logos/'+clientes[i+1].url+'.jpg"></div><div>'+clientes[i+1].nombre+'</div></div></li>'+
+                        '<li onclick="iframe(\''+clientes[i+2].url+'\')"><div class="cliente valign"><div style="background: #fff"><img style="width: 100%" src="http://'+clientes[i+2].url+'/images/logos/'+clientes[i+2].url+'.jpg"></div><div>'+clientes[i+2].nombre+'</div></div></li>'+
+                        '<li onclick="iframe(\''+clientes[i+3].url+'\')"><div class="cliente valign"><div style="background: #fff"><img style="width: 100%" src="http://'+clientes[i+3].url+'/images/logos/'+clientes[i+3].url+'.jpg"></div><div>'+clientes[i+3].nombre+'</div></div></li>'+
+                    '</ul>'+
+                '</div>';
+        
+        $('.cont_show').append(html);
+        
+    }
+    
+}
+function pagina(n){
+    
+    var velocidad = 700;
+    var distancia = 100;
+    
+    if(n == 0){
+        $('.pag01').animate({ opacity: 1, top: '0px' }, velocidad);
+        $('.pag02').animate({ opacity: 0, top: distancia+'%' }, velocidad);
+        $('.pag03').animate({ opacity: 0, top: distancia+'%' }, velocidad);
+    }
+    if(n == 1){
+        $('.pag01').animate({ opacity: 0, top: distancia+'%' }, velocidad);
+        $('.pag02').animate({ opacity: 1, top: '0px' }, velocidad);
+        $('.pag03').animate({ opacity: 0, top: distancia+'%' }, velocidad);
+    }
+    if(n == 2){
+        $('.pag01').animate({ opacity: 0, top: distancia+'%' }, velocidad);
+        $('.pag02').animate({ opacity: 0, top: distancia+'%' }, velocidad);
+        $('.pag03').animate({ opacity: 1, top: '0px' }, velocidad);
+    }    
+    
+}
+function preview_cliente(){
+    var top = $('.cont_show').position().top;
+    if(top < 0){
+        $('.cont_show').animate({top: '+=100%'});
+    }
+}
+function proximo_cliente(){
+    
+    var top = $('.cont_show').position().top;
+    var aux = Math.abs(top / $('.cont_show').height());
+    var cant = $('.cont_show').find('.list_cli').length;
+    
+    if(aux + 1 < cant){
+        $('.cont_show').animate({top: '-=100%'});
+    }
+   
+}
+function iframe(src){
+    $('#telefono').attr('src', 'http://'+src);
+}
+function send(){
+
+    var send = { accion: 'crear_dominio', dominio: $('#dominio').val(), correo: $('#correo').val() };
+    $.ajax({
+        url: "http://35.185.64.95/ajax/index.php",
+        type: "POST",
+        data: send,
+        success: function(info){
+            var data = JSON.parse(info);
+            console.log(data);
+            if(data.op == 1){
+                //$('#dominio').val("");
+                //$('#correo').val("");
+                $('#mensaje').html("Felicitaciones! Ya eres parte de Delivery Shop. Te hemos enviado un correo con las instrucciones")
+            }
+            if(data.op == 2){
+                console.log("ERROR");
+                console.log(data.mensaje);
+            }
+        }, error: function(e){
+            console.log(e);
+        }
+    });
+    
+}
+function a(){
+    return null;
+}
+function contacto(){
+    
+    if($('#nombre').val() != "" && ($('#correo').val() != "" || $('#phone').val() != "") && $('#asunto').val()){
+        var send = { accion: 'contacto', nombre: $('#nombre').val(), correo: $('#correo').val(), phone: $('#phone').val(), asunto: $('#asunto').val() };
+        $.ajax({
+            url: "http://35.185.64.95/ajax/index.php",
+            type: "POST",
+            data: send,
+            success: function(info){
+                var data = JSON.parse(info);
+                console.log(data);
+                if(data.op == 1){
+                    $('#nombre').val('');
+                    $('#correo').val('');
+                    $('#phone').val('');
+                    $('#asunto').val('');
+                }
+                if(data.op == 2){
+                    console.log("ERROR");
+                    console.log(data.mensaje);
+                }
+            }, error: function(e){
+                console.log(e);
+            }
+        });
+    }else{
+        console.log("DEBE LLENAR LOS CAMPOS");
+        return false;
+    }
+    
+    
+}
