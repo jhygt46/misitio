d904ac683c627ad310870e64d1a99822f1cbfa92
jhygt46 <infolifeoperacional@gmail.com>
2019-01-26 23:00:35 -0300

    aa

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
             </div>
         </div>
         
-        <div class="telefono" style="width: 273px; height: 530px; position: absolute; top: 120px">
+        <div class="telefono" style="width: 273px; height: 530px; position: absolute; top: 110px">
             <div style="width: 100%; height: 100%; position: relative">
                 <img style="position: absolute; z-index: 1; width: 273px; height: 530px" src="images/celular.png" alt="" />
                 <iframe style="position: absolute; z-index: 2; width: 241px; height: 430px; border: 0px; padding: 52px 16px 0px 16px" src="http://www.mikasushi.cl"></iframe>
